d7d543f2f6cdc728e4d76d33df7ac337ccccb894
Nikita8121 <natanalegarh@gmail.com>
2023-01-15 16:34:31 +0200

    upd

--- proxy-server/App.xaml.cs
+++ proxy-server/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proxy_server.Services;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace proxy_server
     /// </summary>
     public partial class App : Application
     {
+        private HttpServer _httpServer;
+
+        public App()
+        {
+            _httpServer = new HttpServer();
+            _httpServer.Start();
+        }
     }
 }
